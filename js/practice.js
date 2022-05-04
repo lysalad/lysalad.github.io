f8649c21bc00c4259111c373e0f2de76fc9fe1fd
lysalad <104557575+lysalad@users.noreply.github.com>
2022-05-04 15:23:21 +0900

    Add files via upload

---
+++ js/practice.js
@@ -0,0 +1,7 @@
+<script>
+    document.querySelector('.mopen').addEventListener('click', function () {
+        console.log('찍네~~~');
+    //자기에게 oo 클래스를 붙여줘
+    this.classList.toggle('oo')
+        })
+</script>
