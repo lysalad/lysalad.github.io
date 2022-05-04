207641a14c43aeb897a988780cb0dd8cc3e1fc1c
lysalad <104557575+lysalad@users.noreply.github.com>
2022-05-04 17:12:16 +0900

    Delete js directory

--- js/practice.js
+++
@@ -1,7 +0,0 @@
-<script>
-    document.querySelector('.mopen').addEventListener('click', function () {
-        console.log('찍네~~~');
-    //자기에게 oo 클래스를 붙여줘
-    this.classList.toggle('oo')
-        })
-</script>
