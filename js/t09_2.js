5849a083d86dca4eb382f619d1609dd915c70f58
lysalad <104557575+lysalad@users.noreply.github.com>
2022-05-10 17:30:40 +0900

    Add files via upload

---
+++ js/t09_2.js
@@ -0,0 +1,16 @@
+$(function () {
+    /////////////////////////////////////
+
+    $('.save_video .dec i:nth-of-type(1)').on('click', function () {
+        $('.container video').trigger('play')
+    })
+
+    $('.save_video .dec i:nth-of-type(2)').on('click', function () {
+        $('.container video').trigger('pause')
+    })
+
+
+
+
+    /////////////////////////////////////
+})
